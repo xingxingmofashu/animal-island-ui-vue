88c773d3c8216eda6291c36f9ab60b62b02704d7
xingxingmofash <xingxingmofashu@outlook.com>
2026-05-19 17:46:46 +0800

    feat(Button): add  icon

--- playground/src/app.vue
+++ playground/src/app.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
 import Button from 'animal-island-vue/runtime/components/Button.vue'
-import Icon from 'animal-island-vue/runtime/components/Icon.vue'
 </script>
 
 <template>
   <div class="fixed inset-0 flex items-center justify-center">
     <Button label="Hello" />
-
   </div>
 </template>
--- src/runtime/components/Button.vue
+++ src/runtime/components/Button.vue
@@ -2,6 +2,7 @@
 import type { ButtonHTMLAttributes } from '../types/html';
 import theme from '../../theme/button'
 import { omit } from '../utils';
+import type { IconProps } from './Icon.vue';
 
 type Button = typeof theme
 export interface ButtonProps extends /* @vue-ignore */ ButtonHTMLAttributes {
@@ -20,6 +21,18 @@ export interface ButtonProps extends /* @vue-ignore */ ButtonHTMLAttributes {
    * @defaultValue 'primary'
    */
   variant?: keyof Button['variants']['variant']
+  /** When `true`, the icon will be displayed on the left side. */
+  leading?: boolean
+  /**
+   * Display an icon on the left side.
+   */
+  leadingIcon?: IconProps['name']
+  /** When `true`, the icon will be displayed on the right side. */
+  trailing?: boolean
+  /**
+   * Display an icon on the right side.
+   */
+  trailingIcon?: IconProps['name']
   disabled?: boolean
   loading?: boolean
   block?: boolean
@@ -30,12 +43,12 @@ export interface ButtonProps extends /* @vue-ignore */ ButtonHTMLAttributes {
 <script setup lang="ts">
 import { computed } from 'vue'
 import { tv } from "tailwind-variants"
+import Icon from './Icon.vue';
 
 const props = withDefaults(defineProps<ButtonProps>(), {
   as: 'button',
   type: 'button'
 })
-
 
 const ui = computed(() => tv(theme)({
   disabled: props?.disabled,
@@ -52,9 +65,19 @@ const ui = computed(() => tv(theme)({
     ui?.base({
       class: [props.ui?.base, props.class],
     })]">
+    <slot :ui="ui" name="leading">
+      <span v-if="leading && leadingIcon" :class="ui.leadingIcon({ class: props.ui?.leadingIcon })">
+        <Icon :name="leadingIcon" />
+      </span>
+    </slot>
     <slot :ui="ui">
       <span v-if="props.label" data-slot="label" :class="ui.label({ class: props.ui?.label })">
         {{ props.label }}
+      </span>
+    </slot>
+    <slot :ui="ui" name="trailing">
+      <span v-if="trailing && trailingIcon" :class="ui.trailingIcon({ class: props.ui?.trailingIcon })">
+        <Icon :name="trailingIcon" />
       </span>
     </slot>
   </component>
