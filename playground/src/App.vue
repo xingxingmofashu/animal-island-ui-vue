d78532ce6722dee7fd93d830a4114b1667fa3471
xingxingmofash <xingxingmofashu@outlook.com>
2026-05-19 11:18:26 +0800

    chore: playground

---
+++ playground/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Button from "../../src/runtime/components/Button.vue"
+</script>
+
+<template>
+  <div class="fixed inset-0 flex items-center justify-center">
+    <Button :variant="'primary'"  >Hello</Button>
+  </div>
+</template>
